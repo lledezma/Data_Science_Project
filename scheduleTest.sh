a3b6a92a3830736a3bf1f8c16a9ebb6c8670c310
lledezma <luis.ledezma@pop.belmont.edu>
2020-11-23 08:25:44 -0600

    updates

--- scheduleTest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# launchd test script
-/Users/luis/anaconda/bin/python /Users/luis/PycharmProjects/WebProjects_LuisLedezma/HotelsJSON.py
-/Users/luis/anaconda/bin/python /Users/luis/PycharmProjects/WebProjects_LuisLedezma/TrivagoJSON.py
-
